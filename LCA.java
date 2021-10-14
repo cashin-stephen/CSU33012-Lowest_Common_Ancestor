a97a3c3834b514a260f61c6d75b6910ecf9c8bfe
cashin-stephen <72919384+cashin-stephen@users.noreply.github.com>
2021-10-14 22:33:14 +0100

    removed pesky comments

--- LCA.java
+++ LCA.java
@@ -1,9 +1,8 @@
-// Java Program for Lowest Common Ancestor in a Binary Tree
-// A O(n) solution to find LCA of two given values n1 and n2
+package testing;
+
 import java.util.ArrayList;
 import java.util.List;
 
-// A Binary Tree node
 class Node {
 	int data;
 	Node left, right;
@@ -14,14 +13,13 @@ class Node {
 	}
 }
 
-public class BT_NoParentPtr_Solution1
+public class LCommonA
 {
 
 	Node root;
 	private List<Integer> path1 = new ArrayList<>();
 	private List<Integer> path2 = new ArrayList<>();
 
-	// Finds the path from root node to given root of the tree.
 	int findLCA(int n1, int n2) {
 		path1.clear();
 		path2.clear();
@@ -39,7 +37,6 @@ public class BT_NoParentPtr_Solution1
 		int i;
 		for (i = 0; i < path1.size() && i < path2.size(); i++) {
 			
-		// System.out.println(path1.get(i) + " " + path2.get(i));
 			if (!path1.get(i).equals(path2.get(i)))
 				break;
 		}
@@ -47,17 +44,12 @@ public class BT_NoParentPtr_Solution1
 		return path1.get(i-1);
 	}
 	
-	// Finds the path from root node to given root of the tree, Stores the
-	// path in a vector path[], returns true if path exists otherwise false
 	private boolean findPath(Node root, int n, List<Integer> path)
 	{
-		// base case
 		if (root == null) {
 			return false;
 		}
 		
-		// Store this node . The node will be removed if
-		// not in path from root to n.
 		path.add(root.data);
 
 		if (root.data == n) {
@@ -72,30 +64,16 @@ public class BT_NoParentPtr_Solution1
 			return true;
 		}
 
-		// If not present in subtree rooted with root, remove root from
-		// path[] and return false
 		path.remove(path.size()-1);
 
 		return false;
 	}
-
-	// Driver code
+	
 	public static void main(String[] args)
 	{
-		BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1();
-		tree.root = new Node(1);
-		tree.root.left = new Node(2);
-		tree.root.right = new Node(3);
-		tree.root.left.left = new Node(4);
-		tree.root.left.right = new Node(5);
-		tree.root.right.left = new Node(6);
-		tree.root.right.right = new Node(7);
-
-		System.out.println("LCA(4, 5): " + tree.findLCA(4,5));
-		System.out.println("LCA(4, 6): " + tree.findLCA(4,6));
-		System.out.println("LCA(3, 4): " + tree.findLCA(3,4));
-		System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
-	
+		Node testNode = new Node(2);
+		System.out.print(testNode.data);
 	}
 }
-// This code is contributed by Sreenivasulu Rayanki.
+
+	
